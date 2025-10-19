fd5ca61d65855fc632f50dfd98d6e121dc923ae5
Cr1s106523 <cr.rojaso@duocuc.cl>
2025-10-19 20:24:04 -0300

    Create Curso.java

---
+++ src/main/java/com/EduTech/cl/EduTech/model/Curso.java
@@ -0,0 +1,45 @@
+package com.EduTech.cl.EduTech.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cursos")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCurso;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false, length = 1000)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @Column(nullable = false)
+    private Float precio;
+
+    @Column(nullable = false)
+    private String estado;
+
+    @OneToMany(mappedBy = "curso", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Inscripcion> inscripciones;
+
+    public void publicar() {
+        this.estado = "DISPONIBLE";
+    }
+
+    public void eliminar() {
+        this.estado = "NO_DISPONIBLE";
+    }
+}
