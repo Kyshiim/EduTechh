69b69ec92d73e91fee7ef924d91a500b2b509364
Cr1s106523 <cr.rojaso@duocuc.cl>
2025-10-21 20:21:45 -0300

    Create CursoService.java

---
+++ src/main/java/com/EduTech/cl/EduTech/service/CursoService.java
@@ -0,0 +1,57 @@
+package com.EduTech.cl.EduTech.service;
+
+import com.EduTech.cl.EduTech.model.Curso;
+import com.EduTech.cl.EduTech.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CursoService {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public List<Curso> getCursos() {
+        return cursoRepository.obtenerCursos();
+    }
+
+    public Curso getCursoId(Integer idCurso) {
+        return cursoRepository.buscarPorId(idCurso);
+    }
+
+    public Curso saveCurso(Curso curso) {
+        return cursoRepository.guardar(curso);
+    }
+
+    public Curso updateCurso(Curso curso) {
+        return cursoRepository.actualizar(curso);
+    }
+
+    public String deleteCurso(Integer idCurso) {
+        cursoRepository.eliminar(idCurso);
+        return "Curso eliminado con éxito";
+    }
+
+    public Curso publicarCurso(Integer idCurso) {
+        Curso curso = cursoRepository.buscarPorId(idCurso);
+        if (curso != null) {
+            curso.publicar();
+            cursoRepository.actualizar(curso);
+            return curso;
+        } else {
+            throw new RuntimeException("Curso no encontrado");
+        }
+    }
+
+    public Curso eliminarCurso(Integer idCurso) {
+        Curso curso = cursoRepository.buscarPorId(idCurso);
+        if (curso != null) {
+            curso.eliminar();
+            cursoRepository.actualizar(curso);
+            return curso;
+        } else {
+            throw new RuntimeException("Curso no encontrado");
+        }
+    }
+}
