f7a978cea887c8d46a56c4dcaccc5e540712a5d3
Cr1s106523 <cr.rojaso@duocuc.cl>
2025-10-21 19:29:19 -0300

    Create Curso.java

---
+++ src/main/java/com/EduTech/cl/EduTech/Model/Curso.java
@@ -0,0 +1,41 @@
+package com.EduTech.cl.EduTech.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cursos")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCurso;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false, length = 1000)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @Column(nullable = false)
+    private Float precio;
+
+    @Column(nullable = false)
+    private String estado;
+
+    public void publicar() {
+        this.estado = "DISPONIBLE";
+    }
+
+    public void eliminar() {
+        this.estado = "NO_DISPONIBLE";
+    }
+}
