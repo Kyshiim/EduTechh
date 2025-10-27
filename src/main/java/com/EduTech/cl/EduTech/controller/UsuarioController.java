8bb13fdd3df1e8dc6dab1b9abe6e26d1455755ad
Alfred_Castr <107264087+Alphareone@users.noreply.github.com>
2025-10-26 21:08:37 -0300

    localconmit

--- src/main/java/com/EduTech/cl/EduTech/controller/UsuarioController.java
+++ src/main/java/com/EduTech/cl/EduTech/controller/UsuarioController.java
@@ -1,58 +1,31 @@
 package com.EduTech.cl.EduTech.controller;
 
-import com.EduTech.model.Usuario;
-import com.EduTech.service.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.EduTech.cl.EduTech.model.Usuario;
+import com.EduTech.cl.EduTech.repository.UsuarioRepository;
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
-    private UsuarioService usuarioService;
+    private UsuarioRepository usuarioRepository;
 
     @GetMapping
     public List<Usuario> getAllUsuarios() {
-        return usuarioService.getAllUsuarios();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long id) {
-        Optional<Usuario> usuario = usuarioService.getUsuarioById(id);
-        return usuario.map(ResponseEntity::ok)
-                       .orElseGet(() -> ResponseEntity.notFound().build());
+        return usuarioRepository.findAll();
     }
 
     @PostMapping
     public Usuario createUsuario(@RequestBody Usuario usuario) {
-        return usuarioService.saveUsuario(usuario);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Usuario> updateUsuario(@PathVariable Long id, @RequestBody Usuario usuarioDetails) {
-        return usuarioService.getUsuarioById(id)
-                .map(usuario -> {
-                    usuario.setNombre(usuarioDetails.getNombre());
-                    usuario.setEmail(usuarioDetails.getEmail());
-                    usuario.setContrasena(usuarioDetails.getContrasena());
-                    Usuario updatedUsuario = usuarioService.saveUsuario(usuario);
-                    return ResponseEntity.ok(updatedUsuario);
-                })
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
-        if (usuarioService.getUsuarioById(id).isPresent()) {
-            usuarioService.deleteUsuario(id);
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return usuarioRepository.save(usuario);
     }
 }
--- src/main/java/com/EduTech/cl/EduTech/repository/UsuarioRepository.java
+++ src/main/java/com/EduTech/cl/EduTech/repository/UsuarioRepository.java
@@ -1,9 +1,8 @@
 package com.EduTech.cl.EduTech.repository;
 
-
-import com.EduTech.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.EduTech.cl.EduTech.model.Usuario;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByEmail(String email);
 }
