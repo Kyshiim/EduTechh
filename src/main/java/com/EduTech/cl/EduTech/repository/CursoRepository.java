16e713705ce34a1568035373881cfc3b31c0225c
Cr1s106523 <cr.rojaso@duocuc.cl>
2025-10-28 21:42:35 -0300

    Update CursoRepository.java

--- src/main/java/com/EduTech/cl/EduTech/repository/CursoRepository.java
+++ src/main/java/com/EduTech/cl/EduTech/repository/CursoRepository.java
@@ -1,5 +1,6 @@
 package com.EduTech.cl.EduTech.repository;
 
+
 import com.EduTech.cl.EduTech.model.Curso;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
@@ -9,12 +10,10 @@ import java.util.List;
 public class CursoRepository {
     private List<Curso> listaCursos = new ArrayList<>();
 
-    
     public List<Curso> obtenerCursos() {
         return listaCursos;
     }
 
-    
     public Curso buscarPorId(Integer idCurso) {
         for (Curso curso : listaCursos) {
             if (curso.getIdCurso().equals(idCurso)) {
@@ -24,13 +23,11 @@ public class CursoRepository {
         return null;
     }
 
-    
     public Curso guardar(Curso curso) {
         listaCursos.add(curso);
         return curso;
     }
 
-    
     public Curso actualizar(Curso curso) {
         for (int i = 0; i < listaCursos.size(); i++) {
             if (listaCursos.get(i).getIdCurso().equals(curso.getIdCurso())) {
@@ -41,7 +38,6 @@ public class CursoRepository {
         return null;
     }
 
-    
     public void eliminar(Integer idCurso) {
         listaCursos.removeIf(curso -> curso.getIdCurso().equals(idCurso));
     }
