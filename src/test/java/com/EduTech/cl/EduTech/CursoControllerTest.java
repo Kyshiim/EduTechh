8373f4bf592dbe5a184ebd4093d31aa38a4b3f7b
Cr1s106523 <cr.rojaso@duocuc.cl>
2025-11-19 21:01:39 -0300

    lo

--- src/test/java/com/EduTech/cl/EduTech/CursoControllerTest.java
+++ src/test/java/com/EduTech/cl/EduTech/CursoControllerTest.java
@@ -5,9 +5,9 @@ import com.EduTech.cl.EduTech.model.Curso;
 import com.EduTech.cl.EduTech.service.CursoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
