fcb39d85b0c5fb6160a1b6e1586b5e73b2403fdc
Cristobal <oyarzuncristobal353@gmail.com>
2025-11-20 16:43:25 -0300

    ,,
    ,

---
+++ src/main/java/com/EduTech/cl/EduTech/Config/DataSeeder.java
@@ -0,0 +1,5 @@
+package com.EduTech.cl.EduTech.Config;
+
+public class DataSeeder {
+
+}
--- src/main/java/com/EduTech/cl/EduTech/Config/OpenAPIConfig.java
+++ src/main/java/com/EduTech/cl/EduTech/Config/OpenAPIConfig.java
@@ -1,5 +1,31 @@
 package com.EduTech.cl.EduTech.Config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAPIConfig {
 
+    @Bean
+    public OpenAPI cursoOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("EduTech API - Cursos")
+                        .description("API REST para gestión de cursos en la plataforma EduTech")
+                        .version("v1")
+                        .contact(new Contact()
+                                .name("Equipo EduTech")
+                                .email("equipo@edutech.cl"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Repositorio / Documentación adicional")
+                        .url("https://github.com/tu-org/edutech")); // ajusta con tu repo real
+    }
 }
